752c74dc6596aedd60a89d46d98935d849cc61ad
Jackie Oh <jmoh3@illinois.edu>
2020-04-21 02:05:57 +0000

    Adapted pbencoder.cpp to encode constraints for false positives, negatives, and # clusters

--- pblib/pbencoder.cpp
+++ pblib/pbencoder.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <fstream>
 #include "PBParser.h"
 #include "pb2cnf.h"
 
@@ -9,58 +9,54 @@ using namespace PBLib;
 
 int main(int argc, char **argv)
 {
-  unordered_map<string,string> options;
-  
-  if (argc > 2)
-  {
-    string tmp;
-    for (int i = 2; i < argc; ++i)
-    {
-	tmp = string(argv[i]);
-	while (tmp[0] == '-')
-	    tmp = tmp.substr(1);	
+  if (argc != 14) {
+    return 1;
+  }
 
-	options[tmp.substr(0,tmp.find("="))] = tmp.find("=") == string::npos ? "" : tmp.substr(tmp.find("=") + 1);
+  PB2CNF pb2cnf;
+
+  int32_t first_fresh_variable = atoi(argv[10]) + atoi(argv[11]);
+  int saved_first_var = first_fresh_variable;
+  int num_clauses = 0;
+
+  char* filename = argv[13];
+
+  cout << filename << endl;
+
+  ofstream write_file;
+  write_file.open(filename);
+
+
+  for (int i = 1; i < argc-1; i+=3) {
+    int32_t start_var = atoi(argv[i]);
+    int32_t num_vars = atoi(argv[i+1]);
+    int k = atoi(argv[i+2]);
+    
+    vector< int32_t > literals;
+
+    for (int i = 0; i < num_vars; i++) {
+      literals.push_back(i+start_var);
     }
-  }
-  
 
-  if (argc < 2)
-  {
-    cout << "usage " << argv[0] << " inputfile [options]" << endl;
-    return -1;
-  }
-  
+    vector<vector<int32_t>> formula;
+    first_fresh_variable = pb2cnf.encodeAtMostK(literals, k, formula, first_fresh_variable) + 1;
 
-  PBConfig config = make_shared<PBConfigClass>();
-  VectorClauseDatabase formula(config);
-  
-  PBParser parser;
-  
-  vector<PBConstraint> constraints = parser.parseFile(argv[1]);
-  
-  if (!parser.isOk())
-  {
-    cout << endl << "c error: could not parse input file" << endl;
-    exit(-1);
-  }
+    for (auto clause : formula)
+    {
+      for (auto lit : clause)
+        write_file << lit << " ";
+      write_file << "0\n";
+    }
 
+    num_clauses += formula.size();
+  }
+  
+  int added_vars = first_fresh_variable - (saved_first_var);
 
-  PB2CNF pb2cnf(config);
-  AuxVarManager auxvars(parser.getMaxVarID() + 1);
-  
-  
-  for(int i = 0; i < (int) constraints.size(); ++i)
-  {
-      pb2cnf.encode(constraints[i], formula, auxvars);
-  }
-  
-  cout << "p cnf " <<  auxvars.getBiggestReturnedAuxVar() << " " << formula.getClauses().size() << endl;
-  
-  for (auto clause : formula.getClauses())
-  {
-    for (auto lit : clause)
-      cout << lit << " ";
-    cout << "0" << endl;
-  }
+  write_file << "+vars= " << added_vars << "\n";
+  write_file << "+clauses= " << num_clauses;
+
+  write_file.close();
+
+  return 0;
 }
